maxgrapps/blockscout

Show "Delegators List" modal window (#2498)
Delegator's reward ratio field is added and calculated as follows:
  - if the pool is validator, snapshotStakers and snapshotRewardPercents
    arrays are fetched from contract, zipped and used for delegators' ratios.
  - if a delegator made a stake during this epoch, they won't be present
    in the arrays above, so they will have `nil` for reward ratio
  - if the pool is a candidate, _potential_ reward is calculated manually,
    while taking into account that pool always receives at least 30%.

The list of delegators is displayed in a modal window with the pool itself
being added as the first row, and currently logged in user moved to top.

No paging is implemented for this table, as AsyncListingLoad component
doesn't support more than one pageable table simultaneously.
Fixing that requires almost full component rewrite, and I think that should
be done in a separate PR.

"Me" tooltip is emulated with text, but should be replaced with SVG image.

apps/explorer/priv/repo/migrations/20190731172720_add_reward_ratio_to_pool_delegators.exs
@@ -1,0 +1,9 @@
+defmodule Explorer.Repo.Migrations.AddBlockRewardToPools do
+  use Ecto.Migration
+
+  def change do
+    alter table(:staking_pools_delegators) do
+      add(:reward_ratio, :decimal, precision: 5, scale: 2)
+    end
+  end
+end

peachwaravich/blockscout-1

apps/explorer/priv/repo/migrations/20190731172720_add_reward_ratio_to_pool_delegators.exs
@@ -1,0 +1,9 @@
+defmodule Explorer.Repo.Migrations.AddBlockRewardToPoolDelegators do
+  use Ecto.Migration
+
+  def change do
+    alter table(:staking_pools_delegators) do
+      add(:reward_ratio, :decimal, precision: 5, scale: 2)
+    end
+  end
+end